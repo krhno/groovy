fa1f3fbf62e9b16a2442495dea3b851054868ec8
Oscar Ruiz <39598776+krhno@users.noreply.github.com>
2023-03-02 16:25:34 -0600

    update start and stop commands

--- vars/environment.groovy
+++ vars/environment.groovy
@@ -2,14 +2,14 @@
 
 def init(){
     print "Starting appium server"
-    //sh 'cd ${env.WORKSPACE}/source/ecom-palacio-custom/ && npx appium --relaxed-security' //Triger appium job or send to background
+    sh 'cd ${env.WORKSPACE}/source/ecom-palacio-custom/ && npx appium --relaxed-security > /dev/null 2>&1 &' //Triger appium job or send to background
     print "Starting Android Emulator"
-    //sh 'emulator -no-snapshot-load -avd Pixel4 -no-boot-anim -netspeed full -netdelay none' //Triger emulator job or send to background
+    sh 'emulator -no-snapshot-load -avd Pixel4 -no-window -no-audio -no-boot-anim -netspeed full -netdelay none > /dev/null 2>&1 &' //Triger emulator job or send to background
 }
 
 def destroy(){
     print "Closing the Appium server"
-    //sh "ps -ef | grep -i 'appium' | awk '{print $2}' | xargs sudo kill -9"
+    sh "pkill -9 -f appium"
     print "Closing All emulators"
-    //sh "adb devices | grep 'emulator' | cut -f1 | while read line; do adb -s $line emu kill; done"
+    sh "adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done"
 }
